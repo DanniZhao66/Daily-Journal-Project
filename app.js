db21355c5e88f92bfcd9fc2a279db16975590e37
apple <apple@appledeMBP-3.home>
2020-12-29 20:17:03 +0000

    Update google callback line in app.js

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/dashboard",
+  callbackURL: "https://protected-reef-52698.herokuapp.com/auth/google/dashboard",
   userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
 
 },
